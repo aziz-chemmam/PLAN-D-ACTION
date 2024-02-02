30a040c260034552a5f7e9f1e18afcf4d8c54277
aziz chemmam <azizchemmam123@gmail.com>
2024-02-02 17:00:56 +0100

    2

--- app/models/Article.php
+++ app/models/Article.php
@@ -1,5 +1,54 @@
 <?php
 
+class Article {
+
+        private $id;
+        private $titre;
+        private $contenu;
+        private $date;
+        private $utilisateurId;
+        
+        public function getId(){
+            return $this->id;
+        }
+        
+        public function setId($id){
+            $this->id = $id;
+        }
+        
+        public function getTitre(){
+            return $this->titre;
+        }
+        
+        public function setTitre($titre){
+            $this->titre = $titre;
+        }
+        
+        public function getContenu(){
+            return $this->contenu;
+        }
+        
+        public function setContenu($contenu){
+            $this->contenu = $contenu;
+        }
+       public function getDate(){
+        return $this->date;
+        }
+        
+        public function setDate($date){
+            $this->date = $date;
+        }
+        
+        public function getUtilisateurId(){
+            return $this->utilisateurId;
+        }
+        
+        public function setUtilisateurId($utilisateurId){
+            $this->utilisateurId = $utilisateurId;
+        }
+       }
+
+
 
 
 ?>
